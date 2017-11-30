b9882d07876e970de84afa6b0f638ffc3fed1e4b
Jeremy <jeremydecola@gmail.com>
2017-11-30 14:58:01 -0500

    Test Program for Server Manifest Algorithm

--- serverman_algo_test.py
+++ serverman_algo_test.py
@@ -1,5 +1,6 @@
 
 function_db = []
+id_list = []
 
 request_1 = "declareAction_kitchen/toaster_start-toast_time_intensity"
 request_2 = "declareAction_kitchen/light1_on_intensity"
@@ -11,9 +12,35 @@ request_7 = "getActions_myID_kitchen"
 request_8 = "getActions_myID_light"
 request_9 =  "getActions_myID_light1"
 request_10 = "getActions_myID_"
+request_11 = "getID_giraffe"
+request_12 = "declareAction_giraffe_makeNoise_howl_chew_bark"
+request_13 = "getID_spoon"
+request_14 = "declareAction_spoon_levitate_force_gravity"
+request_15 = "getID_giraffe"
+
+
+
+def getID(function_db, requester_id):
+    print(requester_id + " is requesting an ID")
+    id_in_use = True
+    i = 2
+    #Create an array that lists all IDs currently in DB
+    for function in range(len(function_db)):
+        id_list.append(function_db[function][0])
+    while(id_in_use):
+        if requester_id not in id_list:
+            id_in_use = False
+        else:
+            requester_id = requester_id + str(i)
+            i+=1
+    assigned_id = requester_id
+    print(assigned_id + " is available.")
+    #serverman_client.publish("lobby", assigned_id, qos=2, retain=True)
+    print("ID published to the lobby")
 
 def declareAction(function_db, actor_id, action, parameters):
     function_db.append([actor_id, action, parameters])
+
 
 def getActions(function_db,requester_id, actor_prefix):
     print("Replying to ID:" + requester_id)
@@ -25,27 +52,39 @@ def getActions(function_db,requester_id, actor_prefix):
             if actor_prefix in function_db[function][0]:
                 reply_message += str(function_db[function][element]) + ","
                 prefix_match = True
-        #serverman_client.publish(client_id, reply_message, qos=2, retain=True)
         if prefix_match == True:
             print(str(function) + ", " + reply_message)
+            #serverman_client.publish(requester_id, reply_message, qos=2, retain=True)
+
+def updateActions(function_db, requester_id, client_function_length):
+    db_length = len(function_db)
+    if client_function_length != db_length:
+        for function in range(client_function_length, db_length):
+            reply_message = ""
+            for element in function_db[function]:
+                reply_message += str(function_db[function][element]) + ","
+            #serverman_client.publish(requester_id, reply_message, qos=2, retain=True)
 
 def getRequest(request_message):
     # Split incoming payload into function elements
     function_elements = request_message.split("_")
     request_type = function_elements[0]
     actor_client_id = function_elements[1]
-    # for a declare request_type
-    request_action = function_elements[2]
-    # for a get request_type
-    requested_client_id = function_elements[2]
-
     if len(function_elements) >= 3:
+        # for a declare request_type
+        request_action = function_elements[2]
+        # for a get request_type
+        requested_client_id = function_elements[2]
+    request_parameters = []
+    if len(function_elements) >= 4:
         request_parameters = function_elements[3:]
 
     if "declareAction" in request_type:
         declareAction(function_db, actor_client_id, request_action, request_parameters)
     elif "getActions" in request_type:
         getActions(function_db, actor_client_id, requested_client_id)
+    elif "getID" in request_type:
+        getID(function_db, actor_client_id)
 
 getRequest(request_1)
 getRequest(request_2)
@@ -63,3 +102,8 @@ getRequest(request_7)
 getRequest(request_8)
 getRequest(request_9)
 getRequest(request_10)
+getRequest(request_11)
+getRequest(request_12)
+getRequest(request_13)
+getRequest(request_14)
+getRequest(request_15)
