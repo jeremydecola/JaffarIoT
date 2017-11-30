f5d1bcfbe134b8b4f153740a434f5cd07499fcc1
Jeremy <jeremydecola@gmail.com>
2017-11-30 15:52:12 -0500

    Server Manifest Client

--- serverman.py
+++ serverman.py
@@ -7,22 +7,22 @@ function_db = []
 id_list = []
 
 def getID(function_db, requester_id):
-    #print(requester_id + " is requesting an ID")
+    print(requester_id + " is requesting an ID")
     id_in_use = True
     i = 2
+    client_name = requester_id
     #Create an array that lists all IDs currently in DB
-    for function in range(len(function_db)):
-        id_list.append(function_db[function][0])
     while(id_in_use):
         if requester_id not in id_list:
             id_in_use = False
         else:
-            requester_id = requester_id + str(i)
+            requester_id = client_name + str(i)
             i+=1
     assigned_id = requester_id
-    #print(assigned_id + " is available.")
+    id_list.append(assigned_id)
+    print(assigned_id + " is available.")
     serverman_client.publish("lobby", assigned_id, qos=2, retain=True)
-    #print("ID published to the lobby")
+    print("ID published to the lobby")
 
 
 def declareAction(function_db, actor_id, action, parameters):
@@ -30,8 +30,8 @@ def declareAction(function_db, actor_id, action, parameters):
 
 
 def getActions(function_db,requester_id, actor_prefix):
-    #print("Replying to ID:" + requester_id)
-    #print("Requesting functions from actors related to IDs with prefix:" + actor_prefix)
+    print("Replying to ID:" + requester_id)
+    print("Requesting functions from actors related to IDs with prefix:" + actor_prefix)
     for function in range(len(function_db)):
         reply_message = ""
         prefix_match = False
@@ -40,7 +40,7 @@ def getActions(function_db,requester_id, actor_prefix):
                 reply_message += str(function_db[function][element]) + ","
                 prefix_match = True
         if prefix_match == True:
-            #print(str(function) + ", " + reply_message)
+            print(str(function) + ", " + reply_message)
             serverman_client.publish(requester_id, reply_message, qos=2, retain=True)
 
 def updateActions(function_db, requester_id, client_function_length):
@@ -58,41 +58,41 @@ def on_message(client, userdata, message):
     topic = message.topic
     quality_of_service = message.qos
     retain_message = message.retain
-
-    #############################################################
-    ###### Examples of an incoming messages for serverman: ######
-    ######   declareAction_clientID_action_param1_param2   ######
-    ######             getActions_clientID_door1           ######
-    ######                 getID_clientName                ######
-    #############################################################
-
-    # Split incoming payload into function elements
-    function_elements = request_message.split("_")
-    request_type = function_elements[0]
-    actor_client_id = function_elements[1]
-
-    if len(function_elements) >= 3:
-        # for a declare request_type
-        request_action = function_elements[2]
-        # for a get request_type
-        requested_client_id = function_elements[2]
-    # request_params can be empty for an action decleration so let's initialize
-    request_parameters = []
-    if len(function_elements) >= 4:
-        request_parameters = function_elements[3:]
-
-    if "declareAction" in request_type:
-        declareAction(function_db, actor_client_id, request_action, request_parameters)
-    elif "getActions" in request_type:
-        getActions(function_db, actor_client_id, requested_client_id)
-    elif "getID" in request_type:
-        getID(function_db, actor_client_id)
-
-    #Display incoming message
+    # Display incoming message
     print("message received ", request_message)
     print("message topic=", topic)
     print("message qos=", quality_of_service)
     print("message retain flag=", retain_message)
+
+    if topic == "serverman":
+        #############################################################
+        ###### Examples of an incoming messages for serverman: ######
+        ######   declareAction_clientID_action_param1_param2   ######
+        ######             getActions_clientID_door1           ######
+        ######                 getID_clientName                ######
+        #############################################################
+
+        # Split incoming payload into function elements
+        function_elements = request_message.split("_")
+        request_type = function_elements[0]
+        actor_client_id = function_elements[1]
+
+        if len(function_elements) >= 3:
+            # for a declare request_type
+            request_action = function_elements[2]
+            # for a get request_type
+            requested_client_id = function_elements[2]
+        # request_params can be empty for an action decleration so let's initialize
+        request_parameters = []
+        if len(function_elements) >= 4:
+            request_parameters = function_elements[3:]
+
+        if "declareAction" in request_type:
+            declareAction(function_db, actor_client_id, request_action, request_parameters)
+        elif "getActions" in request_type:
+            getActions(function_db, actor_client_id, requested_client_id)
+        elif "getID" in request_type:
+            getID(function_db, actor_client_id)
 
 #Broker Address
 broker_address = "ec2-34-207-65-122.compute-1.amazonaws.com"
@@ -104,10 +104,14 @@ serverman_client.on_message=on_message # attach function to callback
 #Connect to the MOSQUITTO on EC2 instance
 serverman_client.username_pw_set("ubuntu", password=None)
 serverman_client.connect(broker_address, 1883, 60)
-serverman_client.loop_start() # start loop to stay connected
+
+#Start loop to stay connected
+serverman_client.loop_start()
+#serverman_client.loop_forever()
 
 #Subscribe to 'test' topic with QOS of 2
 serverman_client.subscribe("serverman", qos=2)
+serverman_client.subscribe("potato", qos=2)
 
-time.sleep(4) # wait
+time.sleep(5000) # wait
 serverman_client.loop_stop() #stop the loop
